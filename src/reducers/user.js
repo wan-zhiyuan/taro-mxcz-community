404f2363f34ccdfe71527e4587ed01e534b312ea
wan-zhiyuan <w13585643941@163.com>
2020-10-21 18:45:13 +0800

    udp

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -8,7 +8,16 @@ const INITIAL_STATE = {
 export default function counter(state = INITIAL_STATE, action) {
   switch (action.type) {
     case USER_INFO:
-      return { ...state, userInfo: action.payload }
+      let newState = JSON.parse(JSON.stringify(state))
+      let newUserInfo = action.payload
+      if (JSON.stringify(newState.userInfo) === JSON.stringify(newUserInfo)) {
+        return state
+      } else {
+        return { ...state, userInfo: action.payload }
+      }
+
+      // ⬇️⬇️⬇️优化前代码⬇️⬇️⬇️
+      // return { ...state, userInfo: action.payload }
     case CITY_INFO:
       return { ...state, city: action.payload }
     default:
