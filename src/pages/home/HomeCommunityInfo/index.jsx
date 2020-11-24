ca42dc661e4b9577b7822b3f18e48d2ce0f049a6
wan-zhiyuan <w13585643941@163.com>
2020-11-24 22:21:44 +0800

    udp

--- src/pages/home/HomeCommunityInfo/index.jsx
+++ src/pages/home/HomeCommunityInfo/index.jsx
@@ -49,22 +49,63 @@ export default function Index(props) {
         })
     }
 
+    function naviTo(href) {
+        if (!userInfo.nickname) {
+            Taro.navigateTo({
+                url: '/subPages1/pages/login/login'
+            })
+            return
+        }
+        switch (href) {
+            case 'mall':
+                Taro.navigateTo({
+                    url: '/subPages5/pages/mall/mall'
+                })
+                return;
+            case 'category':
+                Taro.navigateTo({
+                    url: '/subPages3/pages/category/category'
+                })
+                return;
+            case 'information':
+                Taro.navigateTo({
+                    url: '/subPages3/pages/information/information'
+                })
+                return;
+            default:
+                break;
+        }
+    }
+
     return (
         <View className='home_community_info'>
             <View className='title'>
                 <Text style={{ marginLeft: '15px' }}>社区资讯</Text>
             </View>
             <View className='content'>
-                <Image
-                    className='content_item_left'
-                    src={middle_navigation[0].image}
-                    mode='scaleToFill'
-                    onClick={naviToMall}></Image>
-                <Image
-                    className='content_item_right'
-                    src={middle_navigation[1].image}
-                    mode='scaleToFill'
-                    onClick={naviToCommunityInfo}></Image>
+                {
+                    middle_navigation.length <= 1
+                        ?
+                        <Image
+                            className='content_item_one'
+                            src={middle_navigation[0].image}
+                            mode='scaleToFill'
+                            onClick={()=>{naviTo(middle_navigation[0].href)}}></Image>
+                        :
+                        <View className='content_item_two'>
+                            <Image
+                                className='content_item_left'
+                                src={middle_navigation[0].image}
+                                mode='scaleToFill'
+                                onClick={()=>{naviTo(middle_navigation[0].href)}}></Image>
+                            <Image
+                                className='content_item_right'
+                                src={middle_navigation[1].image}
+                                mode='scaleToFill'
+                                onClick={()=>{naviTo(middle_navigation[1].href)}}></Image>
+                        </View>
+                }
+
             </View>
         </View>
     )
